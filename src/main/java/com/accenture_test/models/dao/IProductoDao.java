fba13cdc7f15b90f73b2b4e4d29ef71f1367276c
cristianufps <cristianarbeby@ufps.edu.co>
2021-04-20 13:54:22 -0500

    Initial commit

---
+++ src/main/java/com/accenture_test/models/dao/IProductoDao.java
@@ -0,0 +1,4 @@
+package com.accenture_test.models.dao;
+
+public class IProductoDao {
+}
---
+++ src/test/java/com/accenture_test/AccentureTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accenture_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccentureTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
